cf68b8aa2772abb63e3beaa5ed0ac988de1cd43c
NukLaochaem <nuklaochaem@gmail.com>
2024-03-21 06:57:42 +0100

    personalised data

--- routes/pictures.js
+++ routes/pictures.js
@@ -10,11 +10,10 @@ router.get("/", requiresAuth(), async function (req, res, next) {
   var params = {
     Bucket: process.env.CYCLIC_BUCKET_NAME,
     Delimiter: "/",
-    Prefix: "public/",
+    Prefix: req.oidc.user.email + "/",
   };
   var allObjects = await s3.listObjects(params).promise();
   var keys = allObjects?.Contents.map((x) => x.Key);
-
   const pictures = await Promise.all(
     keys.map(async (key) => {
       let my_file = await s3
@@ -29,27 +28,37 @@ router.get("/", requiresAuth(), async function (req, res, next) {
       };
     })
   );
-
   res.render("pictures", { pictures: pictures });
 });
 
 router.post("/", requiresAuth(), async function (req, res, next) {
   const file = req.files.file;
   console.log(req.files);
-
   await s3
     .putObject({
       Body: file.data,
       Bucket: process.env.CYCLIC_BUCKET_NAME,
-      Key: "public/" + file.name,
+      Key: req.oidc.user.email + "/" + file.name,
     })
     .promise();
-
   res.end();
 });
 
-router.get("/:pictureName", function (req, res, next) {
-  res.render("pictureDetails", { picture: req.params.pictureName });
-});
+/*
+router.get("/:pictureName", requiresAuth(), async function (req, res, next) {
+  const my_file = await s3
+    .getObject({
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: "public/" + req.params.pictureName,
+    })
+    .promise();
+
+  const picture = {
+    src: Buffer.from(my_file.Body).toString("base64"),
+    Name: req.params.pictureName,
+  };
+
+  res.render("pictureDetails", { picture: picture });
+});*/
 
 module.exports = router;
